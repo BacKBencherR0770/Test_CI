4d691bf290ceff58447604e6a5ed808687909e43
Backbencher <backbencher0770@gmail.com>
2026-03-16 12:04:44 +0530

    Fix formatting in app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
- app = Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, DevOps Students!"
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
